0796a674f139aed9f86cc234a232d7b898250708
unknown <4purposeliving@gmail.com>
2018-09-18 09:20:49 -0700

    update

--- wp-content/themes/charitypro/includes/modules/shortcodes/call_to_action.php
+++
@@ -1,8 +0,0 @@
-<!--Call To Action-->
-<section class="call-to-action" style="background-image:url(<?php echo esc_url($bg_img);?>);">
-    <div class="auto-container">
-        <h2><?php echo balanceTags($sub_title);?></h2>
-        <h1><?php echo balanceTags($title);?></h1>
-        <a href="<?php echo esc_url($btn_link);?>" class="theme-btn btn-style-two"><?php echo balanceTags($btn_title);?></a>
-    </div>
-</section>
